fc96a33d304cbef24b9fdd55087d38f7c6071aa9
Victor Santos <jvic71174@gmail.com>
2021-10-19 21:33:12 -0300

    auth header aplicado a main route

---
+++ src/app/routes/user/login.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+
+const bcrypt = require('bcryptjs');
+
+const User = require('../../models/user');
+const generateToken = require('../../controllers/authController');
+
+router.post('/login', async (req, res) => {
+
+    const { email, password } = req.body;
+  
+    const user = await User.findOne({ email }).select('+password');
+  
+    if(!user) 
+     return res.status(400).send({ error: 'User not found'});
+    
+    if(!await bcrypt.compare(password, user.password))
+      return res.status(400).send({ error: 'Invalid password'});
+  
+    user.password = undefined;
+    
+    res.send({ 
+      user, 
+      token: generateToken({ id: user.id })
+    });
+  
+  });
+  
+  module.exports = app => app.use(router);
+  
--- src/server.js
+++ src/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const app = express();
+const routes = require('./app/routes/routes');
 
 const PORT = 3080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-require('./app/controllers/projectController')(app);
-require('./app/routes/routes')(app);
+app.use(routes);
 
 app.listen(process.env.PORT || PORT, () => {
    console.log(`Servidor ativo na porta: ${PORT}`);
