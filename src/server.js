d2da3a30eb698666d6209b44f132d67f539e16ae
Victor Santos <jvic71174@gmail.com>
2021-10-27 22:35:30 -0300

    Ajuste

--- src/server.js
+++ src/server.js
@@ -1,16 +1,14 @@
 const express = require('express');
 const app = express();
-const routes = require('./app/routes/routes');
-const cors = require('cors')
 
-app.use(cors());
+const cors = require('cors')
 
 const PORT = 3080;
 
+app.use(cors());
 app.use(express.json());
+app.use(require('./app/routes/routes'));
 app.use(express.urlencoded({ extended: false }));
-
-app.use(routes);
 
 app.listen(process.env.PORT || PORT, () => {
    console.log(`Servidor ativo na porta: ${PORT}`);
