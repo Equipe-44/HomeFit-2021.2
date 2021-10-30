3ed583b3ccfb267a4c5469bde8dbe31124c31de9
Victor Santos <jvic71174@gmail.com>
2021-10-30 18:33:40 -0300

    df

--- src/app/routes/routes.js
+++ src/app/routes/routes.js
@@ -5,50 +5,16 @@ const router = express.Router();
 
 // *********** CADASTRO ***********
 
-// require('./user/register')(router);
-
-// const express = require('express');
-// const router = express.Router();
-
-const User = require('../models/user');
-const generateToken = require('../controllers/authController');
-
-router.post('/register', async (req, res) => {
-
-    const { email } = req.body;
- 
-    try {
-     
-     if(await User.findOne({ email })) {
-       
-      return res.status(400).send({ error: 'User already exists' });
-       
-     } else {
- 
-      const user = await User.create(req.body);
-      user.password = undefined;
- 
-      return res.send({ 
-        user,
-        token: generateToken({ id: user.id })
-     });
- 
-     }
- 
-    } catch (err) {
-      
-      return res.status(400).send({ error: err });
-    }
- });
+require('./user/register')(router);
 
 // *********** LOGIN ***********
 
-// require('./user/login')(router);
+require('./user/login')(router);
 
 
 // *********** HOME ***********
 
-// require('./workouts/home')(router);
+require('./workouts/home')(router);
 
 
 module.exports = router;
