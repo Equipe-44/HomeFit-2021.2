cfaeb83dd43d5432a839fcf1abcd064f112942c8
Victor Santos <jvic71174@gmail.com>
2021-10-31 20:50:02 -0300

    gfh

--- src/server.js
+++ src/server.js
@@ -3,20 +3,6 @@ const app = express();
 
 // const mongoose = require('./dataBase/config')
 
-require("dotenv").config();
-
-const mongoose = require('mongoose');
-
-mongoose.connect(
-  process.env.MONGO_URL,
-  {
-    useNewUrlParser: false,
-  }
-).then(() => console.log('MongoDB connected...'))
-.catch(err => console.log(err));
-
-mongoose.Promise = global.Promise;
-
 const cors = require('cors')
 
 const PORT = 3080;
