e1b71bf268f581946552e88af6b80c6e4b8b3e57
Victor Santos <jvic71174@gmail.com>
2021-11-01 22:12:19 -0300

    j

--- src/dataBase/config.js
+++ src/dataBase/config.js
@@ -2,19 +2,16 @@ require("dotenv").config();
 
 const mongoose = require('mongoose');
 
-// function connectToDatabase() {
-
 mongoose.connect(
+  
   process.env.MONGO_URL,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true
   }
-).then(() => console.log('MongoDB connected...'))
-.catch(err => console.log(err));
+
+).then(() => console.log('MongoDB connected...')).catch(err => console.log(err));
 
 mongoose.Promise = global.Promise;
 
-// }
 module.exports = mongoose;
-// module.exports = app => app.use(mongoose);
--- src/server.js
+++ src/server.js
@@ -4,9 +4,6 @@ const app = express();
 
 const PORT = 3080;
 
-// const connectToDatabase = require('./dataBase/config');
-// connectToDatabase();
-
 app.use(cors());
 app.use(express.json());
 app.use(require('./app/routes/routes'));
